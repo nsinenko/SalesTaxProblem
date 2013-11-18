ac2fcd3ba2bc4258137a492635f53f42af962e10
Nick Sinenko <nick@envit.com.au>
2013-11-18 20:05:32 +1100

    create tests for Receipt

---
+++ spec/receipt_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+describe Receipt do
+	context "receipt with four items" do
+		before (:all) do
+			products = [
+				Product.new(name: "imported bottle of perfume", price: 1.50),
+				Product.new(name: "bottle of perfume", price: 1.75),
+				Product.new(name: "packet of headache pills", price: 1.90),
+				Product.new(name: "box of imported chocolates", price: 1.65)
+			]
+
+			@receipt = Receipt.new
+
+			products.each_with_index do |product, index|
+				@receipt.add_item ReceiptItem.new(quantity: index + 1, product: product)
+			end
+	    end
+
+	    describe "#new" do
+	    	it "returns a Receipt object" do
+		        expect(@receipt).to be_an_instance_of Receipt
+		    end
+
+	       	it "has four items" do
+		        expect(@receipt).to have(4).items
+		    end
+
+		    it "returns a correct total" do
+		    	expect(@receipt.total).to eq 17.30
+		    end
+	    end
+	end
+
+	context "receipt with zero items" do
+		subject(:receipt) { Receipt.new }
+
+		describe "#new" do
+			it { should be_an_instance_of Receipt }
+
+			it { should have(0).items }
+
+		    it "returns a zero total" do
+		    	expect(receipt.total).to eq 0
+		    end
+		end
+	end
+end
