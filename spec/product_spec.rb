e226f0166c1a651bdf83952d67c74f2562ee7aac
Nick Sinenko <nick@envit.com.au>
2013-11-18 18:57:12 +1100

    create tests for the Product class

---
+++ spec/product_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe Product do
+	before :each do
+		@product = Product.new(:name => 'Milk', :price => 2.00)
+	end
+
+	describe "#new" do
+	    it "returns a Product object" do
+	        @product.should be_an_instance_of Product
+	    end
+
+	    it "returns a correct name" do
+	    	@product.name.should.eql('Milk')
+	    end
+
+	    it "returns a correct price" do
+	    	@product.price.should eql(2.00)
+		end
+	end
+
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,4 @@
+require_relative '../product'
+require_relative '../receipt_item'
+require_relative '../receipt'
+require_relative '../tax'
