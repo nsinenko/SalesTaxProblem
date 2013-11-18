037c64e73954d9752a2f7587a9494445cd36b81c
Nick Sinenko <nick@envit.com.au>
2013-11-18 21:03:19 +1100

    refactor Tax tests

--- spec/tax_spec.rb
+++ spec/tax_spec.rb
@@ -1,34 +1,40 @@
 require 'spec_helper'
 
 describe Tax do
-	def tax(name)
+	def tax_on(name)
 		product = Product.new(name: name, price: 10)
 		item = Item.new(quantity: 2, product: product )
 		Tax.new(item)
 	end
 
+	describe "new" do
+		it "return a tax object" do
+		    expect(tax_on("name")).to be_an_instance_of Tax
+	    end
+	end
+
 	describe "total_tax" do
 		context "when products are tax-free" do
-			it "returns zero tax on food" do
-				tax("box of chocolates").total_tax.should eq 0
+			it "returns a zero tax on food" do
+				tax_on("box of chocolates").total_tax.should eq 0
 			end
 
-			it "returns zero tax on books" do
-				tax("book").total_tax.should eq 0
+			it "returns a zero tax on books" do
+				tax_on("book").total_tax.should eq 0
 			end
 
-			it "returns zero tax on medical" do
-				tax("pills").total_tax.should eq 0
+			it "returns a zero tax on medical" do
+				tax_on("pills").total_tax.should eq 0
 			end
 		end
 
 		context "when product are taxable" do 
 			it "returns correct sales tax on" do
-				tax("bottle of perfume").total_tax.should eq 2.00
+				tax_on("bottle of perfume").total_tax.should eq 2.00
 			end
 
 			it "returns correct sales tax including import duty" do
-				tax("bottle of imported perfume").total_tax.should eq 2.10
+				tax_on("bottle of imported perfume").total_tax.should eq 2.10
 			end
 		end
 	end
